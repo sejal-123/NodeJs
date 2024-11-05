04139492d8f2c2619554a8b6597714136154d943
sejal-123 <sejalbudhani@gmail.com>
2024-11-05 20:35:52 +0530

    Dynamic record insertion and fetching data based on email and id

--- src/DatabaseConnection.js
+++ src/DatabaseConnection.js
@@ -3,20 +3,58 @@ const app = express();
 const connectDB = require('./config/database')
 const User = require('./models/user');
 
+// converting the json from the req body to a js object
+app.use(express.json());
 app.post('/signup', async (req, res) => {
+    console.log(req.body);
     // creating a new instance of the User model
-    const user = new User({
-        firstName: 'Sejal',
-        lastName: 'Budhani',
-        emailId: 'test@t.com',
-        password: 'test123',
-        age: 10
-    });
+    const user = new User(req.body);
     try {
         await user.save();
         res.send('User added successfully');
     } catch(e) {
         res.status(400).send('Error saving user' + e.message);
+    }
+});
+
+// Get user by email
+app.get('/user', async (req, res) => {
+    console.log(req.body.emailId);
+    try{
+        // const users = await User.findOne({ emailId: req.body.emailId });
+        const users = await User.find({ emailId: req.body.emailId });
+        if (users.length > 0) {
+            res.send(users);
+        } else {
+            res.send('User not found');
+        }
+    } catch(e) {
+        res.send('Something went wrong');
+    }
+});
+
+// Get user by id
+app.get('/userById', async (req, res) => {
+    console.log(req.body.id);
+    try{
+        const user = await User.findById(req.body.id);
+        if (user) {
+            res.send(user);
+        } else {
+            res.send('User not found');
+        }
+    } catch(e) {
+        res.send('Something went wrong');
+    }
+});
+
+// Get all users
+app.get('/feed', async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch(e) {
+        res.send('Something went wrong');
     }
 });
 
